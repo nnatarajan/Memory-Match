a038fadfdfa3825d12d56aae32f20745acfe9fa3
Natasha Natarajan <natashanatarajan@gmail.com>
2017-06-18 23:10:09 -0600

    working game

--- js/memory.js
+++ js/memory.js
@@ -1,6 +1,46 @@
-$('p').hide() 
+/* Program starts with all cards are hidden allows user to click on two cards. 
+** If there there is a match, the cards remain revealed for the rest of the game.
+** Otherwise, the cards are hidden again.
+** Game ends when all cards are revealed.
+*/
+
+// Hide all card values at start of game
 
 
+// Load JS after DOM loaded
+$(document).ready(function () {
+$('p').hide();
+var pairs = 0;
+var numShowing = 0;
+var showArray = [];
+var match, gameOver;
 
-
-    
+        $(".card").click(function(){
+            if (numShowing <= 1){
+                $(this).find('p').show().text();
+                showArray.push($(this))
+                numShowing +=1;x
+            }
+            if (numShowing === 2){
+                // check for match
+                if (showArray[0].find('p').text() == showArray[1].find('p').text()){
+                    // reinitialize comparision array
+                    pairs += 1;
+                }      
+                else {
+                    showArray.forEach(function(element) {
+                        element.find('p').hide()
+                    });
+                }
+                showArray = [];
+                numShowing = 0;
+            }
+         // decide when game ends, when there are eight pairs  
+         // after clicking on, reveal card value       
+            if (pairs === 8) {
+            document.write("Congrats!");
+            document.write("You won!");
+            return
+            } 
+        }); 
+});
