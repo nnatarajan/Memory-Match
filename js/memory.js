fd83f1df9d6427882dd32fac465015de97122f53
Natasha Natarajan <natashanatarajan@gmail.com>
2017-06-22 12:06:43 -0600

    comments

--- js/memory.js
+++ js/memory.js
@@ -1,18 +1,20 @@
-/* Program starts with all cards are hidden allows user to click on two cards. 
-** If there there is a match, the cards remain revealed for the rest of the game.
-** Otherwise, the cards are hidden again.
-** Game ends when all cards are revealed.
+/* Program simulates memory game.
+** At the start, all card values are hidden. User selects two cards by clicking on them. 
+** If a match is found, the matched cards' values remain revealed for the rest of the game.
+** Otherwise, selected card values are rehidden. Game ends when all matches are made.
 */
-
 
 // Load JS after DOM loaded
 $(document).ready(function () {
-// Hide all card values at start of game
-$('p').hide();
+    
+// Initialize variables
 var pairs = 0;
 var numShowing = 0;
 var showArray = [];
 var match, gameOver;
+
+// Hide all card values at start of game
+$('p').hide();
 
          // after clicking on, reveal card value  
         $(".card").click(function(){
@@ -21,17 +23,18 @@ var match, gameOver;
                 showArray.push($(this))
                 numShowing +=1;x
             }
+            // if two cards are selected, check for a match
             if (numShowing === 2){
-                // check for match
                 if (showArray[0].find('p').text() == showArray[1].find('p').text()){
                     pairs += 1;
-                }      
+                }  
+                // if no match is found, rehide card values    
                 else {
                     showArray.forEach(function(element) {
                         element.find('p').hide()
                     });
                 }
-                // reinitialize comparision array
+                // reinitialize comparision array and number of pairs showing
                 showArray = [];
                 numShowing = 0;
             }
